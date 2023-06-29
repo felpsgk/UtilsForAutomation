8579e8fbcf3080fba56aaa1ee0c583a0082921a4
Felipe <contatofemachado@gmail.com>
2023-06-29 19:53:15 -0300

    screenshot

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,13 @@
+import org.apache.commons.io.FileUtils;
 import org.example.DriverFactory;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,8 +16,12 @@ public class MainTest {
     public static Dimension newDimension = new Dimension(1300, 800);
 
     @Test
-    public void initTest() throws InterruptedException {
+    public void initTest() throws InterruptedException, IOException {
         driver = DriverFactory.getDriver("chrome");
         driver.get("https://www.google.com");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // Now you can do whatever you need to do with it, for example copy somewhere
+        FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+        driver.quit();
     }
 }
