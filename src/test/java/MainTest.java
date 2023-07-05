bf0fc171fcf70455139ff0320749bc8b7e0df077
Felipe <contatofemachado@gmail.com>
2023-07-05 07:44:39 -0300

    variar tempo para batida de ponto

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
 import org.openqa.selenium.*;
+
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Random;
+
 public class MainTest {
     //teste
     static WebDriver driver;
@@ -9,22 +13,37 @@ public class MainTest {
     By campoSenha = By.xpath("//div[contains(@class,'inside')]//input[contains(@type,'pass')]");
     By entrar = By.xpath("//span[contains(text(),'Entrar')]");
     By batePonto = By.xpath("(//span[contains(@class,'pm-button-content') and contains(text(),'Bater ponto')])[3]");
+
     @Test
     public void initTest() throws InterruptedException, IOException {
         driver = DriverFactory.getDriver("chrome");
         utils = new Utils(driver);
         driver.get("https://app2.pontomais.com.br/registrar-ponto");
         System.out.println("ENTROU SITE");
-        if(utils.returnIfElementExistsByTime(campoEmail,10)){
+        if (utils.returnIfElementExistsByTime(campoEmail, 10)) {
             System.out.println("PRECISOU LOGAR");
-            utils.clearAndSendKeys(campoEmail,"felipe.machado@proversolucoes.com.br");
-            utils.clearAndSendKeys(campoSenha,"84141398aB");
+            utils.clearAndSendKeys(campoEmail, "felipe.machado@proversolucoes.com.br");
+            utils.clearAndSendKeys(campoSenha, "84141398aB");
             utils.clicaBy(entrar);
         }
         utils.waitAtime(1);
         utils.clicaTexto("Utilizar essa localização");
         System.out.println("CLICOU LOCALIZAÇÃO");
-        utils.waitAtime(1);
+
+        int[] values = {60, 120, 180, 240, 300};
+        LocalTime currentTime = LocalTime.now();
+
+        if (currentTime.isBefore(LocalTime.of(11, 0)) || currentTime.isAfter(LocalTime.of(15, 0))) {
+            Random random = new Random();
+            int index = random.nextInt(values.length);
+            int randomValue = values[index];
+            System.out.println("RANDOM VAI ESPERAR " + randomValue / 60 + " minutos");
+            utils.waitAtime(randomValue);
+        } else {
+            System.out.println("dentro de meio dia, vai esperar 3 segundos");
+            utils.waitAtime(3);
+        }
+
         utils.clicaBy(batePonto);
         System.out.println("BATEU PONTO -- ESPERANDO REGISTRAR O PONTO");
         utils.waitAtime(10);
